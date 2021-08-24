bf2f5591a6b802f956b85c63e315387b9c29a81e
jennafritz <jennafritz24@gmail.com>
2021-08-24 12:47:27 -0400

    Completed JS

--- javascript/validBTS.js
+++ javascript/validBTS.js
@@ -8,6 +8,42 @@ class Node {
 
 function isValidBST(root) {
   // type your code here
+  if(root === null){
+    return
+  }
+  let queue = []
+  queue.push(root)
+
+  let validityCheck = (node) => {
+    if(root.left !== null){
+      if(root.left.val >= root.val){
+        return false
+      }
+    }
+    if(root.right !== null){
+      if(root.right.val <= root.val){
+        return false
+      }
+    }
+    return true
+  }
+
+  if(validityCheck(root)){
+    queue.shift()
+    if(root.right !== null){
+      queue.unshift(root.right)
+    }
+    if(root.left !== null){
+      queue.unshift(root.left)
+    }
+    if(queue.length > 0){
+      return isValidBST(queue[0])
+    } else {
+      return true
+    }
+  } else {
+    return false
+  }
 }
 
 if (require.main === module) {
@@ -17,6 +53,11 @@ const T = (v, l, r) => new Node(v, l, r)
 
 console.log("Expecting: true");
 console.log("=>", isValidBST(T(2, T(1), T(3))));
+
+console.log("")
+
+console.log("Expecting: false")
+console.log("=>", isValidBST(T(5, T(1), T(4, T(3), T(6)))))
 
 }
 
